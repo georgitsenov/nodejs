776c6b86ea135631dd5ff7170f7b139cdceaade7
Georgi Tsenov <georgi.tsenov.90@gmail.com>
2020-03-02 17:21:24 +0200

    Created / and /admin entry points and way to authenticate with valid user

--- app.js
+++ app.js
@@ -4,4 +4,49 @@ var session = require('express-session');
 var bodyParser = require('body-parser');
 var path = require('path');
 
+var connection = require('./db');
 
+var app = express();
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+app.get('/', function(request, response) {
+	response.send("POSTS");
+});
+
+app.get('/admin', function(request, response) {
+    if (request.session.loggedin) {
+        response.send('Welcome back, ' + request.session.username + '!');
+    } else {
+		response.sendFile('admin.html', {root: __dirname });
+    }   
+    //response.end();
+});
+
+app.post('/admin', function(request, response) {
+	var email = request.body.email;
+	var password = request.body.password;
+	if (email && password) {
+		connection.query('SELECT * FROM users WHERE email = ? AND password = ?', [email, password], function(error, results, fields) {
+			if (results.length && results.length > 0) {
+				request.session.loggedin = true;
+				request.session.email = email;
+				response.redirect('/admin');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+
+app.listen(3000);
