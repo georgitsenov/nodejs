20c908c0327959d5306b3379ca07c6b16a1bfb02
Georgi Tsenov <georgi.tsenov.90@gmail.com>
2020-03-09 00:42:32 +0200

    express-rate-limit implemented for /admin route

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ var express = require('express');
 var session = require('express-session');
 var mysql = require('mysql');
 var path = require('path');
+var rateLimit = require("express-rate-limit");
 
 var app = express();
+
+var adminLimiter = rateLimit({
+	max: 5,
+	windowMs: 60000,
+	message: 'Too many requests, please try again later.',
+	statusCode: 429,
+	headers: true
+
+});
 
 app.use(session({
 	secret: ';=fjGnwsV`2+#=9Y',
@@ -12,6 +22,7 @@ app.use(session({
 }));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use('/admin', adminLimiter)
 
 app.use('/', require('./routes'));
 app.use('/admin', require('./routes/admin'));
